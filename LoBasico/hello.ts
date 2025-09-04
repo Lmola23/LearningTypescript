116d672b379c0db9d4f35d043f5d6752f1dec873
Luis Martin Mola Enriquez <molaenriquezl@gmail.com>
2025-09-04 17:50:27 -0400

    compilandojs

---
+++ LoBasico/hello.ts
@@ -0,0 +1,18 @@
+console.log('Hello Word');
+//Si queremos activar el modo estricto de ts para que no compile el codigo si hay errores tipografico utlizaremos el comando tsc --noEmitOnError [nombre del archivo]
+
+//Comprobando la deteccion de errores de ts
+/*function greet(person, date) {
+  console.log(`Hello ${person}, today is ${date}!`);
+}
+ 
+greet("Brendan");*/
+
+function greet(person : string , date:Date) :void {
+  console.log(`Hello ${person}, today is ${date.toDateString()}`)
+};
+greet("Luis", new Date());
+//en ts avese puede inferir el tipo y no hay que escribir la notacion explicitamente 
+
+
+
